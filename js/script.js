70aa992457ada9e34ccaea722a222a087c39532b
taes60711 <taes60711@gmail.com>
2025-09-27 18:38:03 +0900

    ボタン機能を与える

---
+++ js/script.js
@@ -0,0 +1,19 @@
+    
+  //// html要素を取得
+  //  document.querySelector(), querySelectorAll()
+  //  参考URL: https://www.sejuku.net/blog/70581
+  ////
+
+
+  /// loginButton　クリック時の処理
+  document.querySelector('.loginButton').addEventListener('click', () => {
+
+    /// ID 要素の値を取得
+    const idValue = document.querySelector('input[placeholder="ID"]').value;
+    /// Password 要素の値を取得
+    const passwordValue = document.querySelector('input[placeholder="Password"]').value;
+
+    /// print to console
+    console.log('ID:', idValue);
+    console.log('Password:', passwordValue);
+  });
